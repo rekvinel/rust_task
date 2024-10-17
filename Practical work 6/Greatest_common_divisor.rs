053320e65ecc338ea085a7dc99488ed587e10849
Viacheslav Smischuk <sonik100sonik@gmail.com>
2024-10-17 11:51:17 +0300

    add Greatest_common_divisor

---
+++ Practical work 6/Greatest_common_divisor.rs
@@ -0,0 +1,15 @@
+fn gcd(mut a: u32, mut b: u32) -> u32 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+fn main() {
+    let num1 = 56;
+    let num2 = 98;
+
+    println!("GCD of {} and {} is {}", num1, num2, gcd(num1, num2));
+}
