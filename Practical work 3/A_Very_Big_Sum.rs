1c5bd30d43901196f853264c1a683e72b5d2095e
Viacheslav Smischuk <sonik100sonik@gmail.com>
2024-10-19 16:02:58 +0300

    add task

---
+++ Practical work 3/A_Very_Big_Sum.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn aVeryBigSum(ar: &[i64]) -> i64 {
+    ar.iter().sum()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _ar_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let ar: Vec<i64> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+
+    let result = aVeryBigSum(&ar);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
