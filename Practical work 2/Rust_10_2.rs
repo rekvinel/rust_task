381f99975e247df5518835a28c74cd8707da0b7d
Viacheslav Smischuk <sonik100sonik@gmail.com>
2024-10-27 10:41:40 +0200

    add Rust_10_1-2

---
+++ Practical work 2/Rust_10_2.rs
@@ -0,0 +1,58 @@
+//1.
+struct Array<T, const N: usize> {
+    data : [T; N]
+}
+
+fn main() {
+    let arrays = [
+        Array{
+            data: [1, 2, 3],
+        },
+        Array {
+            data: [1, 2, 3],
+        },
+        Array {
+            data: [1, 2, 4]
+        }
+    ];
+}
+
+//2.
+fn print_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+    println!("{:?}", arr);
+}
+fn main() {
+    let arr = [1, 2, 3];
+    print_array(arr);
+
+    let arr = ["hello", "world"];
+    print_array(arr);
+}
+
+//3.
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+
+fn check_size<T>(val: T)
+where
+    Assert<{ core::mem::size_of::<T>() < 768 }>: IsTrue,
+{
+    //...
+}
+
+// fix the errors in main
+fn main() {
+    check_size([0u8; 767]); 
+    check_size([0i32; 191]);
+    check_size(["hello你好"; 47]); 
+    check_size([(); 31].map(|_| "hello你好".to_string()));  
+    check_size(['中'; 191]); 
+}
+
+
+
+pub enum Assert<const CHECK: bool> {}
+
+pub trait IsTrue {}
+
+impl IsTrue for Assert<true> {}
